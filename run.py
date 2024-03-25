3f26e3de8bc7e0e4c4c901fe1f820cc53f8e1727
your_username <tranxuantruong203hn@gmail.com>
2024-03-25 10:41:27 +0700

    deploy

--- run.py
+++ run.py
@@ -10,31 +10,31 @@ def covert_coco_to_pascal_voc(coco):
     y_max = y_min + height
     return [x_min, y_min, x_max, y_max]
 
-# gdown.download("https://drive.google.com/uc?id=134QOvaatwKdy0iIeNqA_p-xkAhkV4F8Y", "CrowdHuman_train01.zip", quiet=False)
-# gdown.download("https://drive.google.com/uc?id=17evzPh7gc1JBNvnW1ENXLy5Kr4Q_Nnla", "CrowdHuman_train02.zip", quiet=False)
-# gdown.download("https://drive.google.com/uc?id=1tdp0UCgxrqy1B6p8LkR-Iy0aIJ8l4fJW", "CrowdHuman_train03.zip", quiet=False)
-# gdown.download("https://drive.google.com/uc?id=18jFI789CoHTppQ7vmRSFEdnGaSQZ4YzO", "CrowdHuman_val.zip", quiet=False)
+gdown.download("https://drive.google.com/uc?id=134QOvaatwKdy0iIeNqA_p-xkAhkV4F8Y", "CrowdHuman_train01.zip", quiet=False)
+gdown.download("https://drive.google.com/uc?id=17evzPh7gc1JBNvnW1ENXLy5Kr4Q_Nnla", "CrowdHuman_train02.zip", quiet=False)
+gdown.download("https://drive.google.com/uc?id=1tdp0UCgxrqy1B6p8LkR-Iy0aIJ8l4fJW", "CrowdHuman_train03.zip", quiet=False)
+gdown.download("https://drive.google.com/uc?id=18jFI789CoHTppQ7vmRSFEdnGaSQZ4YzO", "CrowdHuman_val.zip", quiet=False)
 
-# with zipfile.ZipFile("CrowdHuman_train01.zip", 'r') as zip_ref:
-#     zip_ref.extractall()
-# with zipfile.ZipFile("CrowdHuman_train02.zip", 'r') as zip_ref:
-#     zip_ref.extractall()
-# with zipfile.ZipFile("CrowdHuman_train03.zip", 'r') as zip_ref:
-#     zip_ref.extractall()
-# with zipfile.ZipFile("CrowdHuman_val.zip", 'r') as zip_ref:
-#     zip_ref.extractall()
+with zipfile.ZipFile("CrowdHuman_train01.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+with zipfile.ZipFile("CrowdHuman_train02.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+with zipfile.ZipFile("CrowdHuman_train03.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+with zipfile.ZipFile("CrowdHuman_val.zip", 'r') as zip_ref:
+    zip_ref.extractall()
 
-# os.mkdir("./data")
-# os.mkdir("./data/images")
-# os.mkdir("./data/labels")
-# for filename in os.listdir('./Images'):
-#     shutil.move(f"./Images/{filename}", "./data/images")
+os.mkdir("./data")
+os.mkdir("./data/images")
+os.mkdir("./data/labels")
+for filename in os.listdir('./Images'):
+    shutil.move(f"./Images/{filename}", "./data/images")
 
-# os.rmdir("Images")
+os.rmdir("Images")
 
-# with open("info.txt", "w") as f_w:
-#     f_w.write("""hbox fbox vbox fbox gender age\nformat: x_min, y_min, x_max, y_max\n""")
-#     f_w.write("""fbox: human full-body bounding-box\nvbox: human visible-region bounding-box\nhbox: head bounding-box\n""")
+with open("info.txt", "w") as f_w:
+    f_w.write("""hbox fbox vbox fbox gender age\nformat: x_min, y_min, x_max, y_max\n""")
+    f_w.write("""fbox: human full-body bounding-box\nvbox: human visible-region bounding-box\nhbox: head bounding-box\n""")
 
 for filename in ["annotation_val_with_classes.csv", "annotation_train_with_classes.csv"]:
     with open(filename) as f_r:
